e80111925957e0d2d33ed567eff310ae56ee56f2
casey-stevens <casey.stevens@cadmusgroup.com>
2017-06-06 18:04:35 -0700

    Working on Item 10, Table 17 - CS

--- Code/Table Code/Items 3-5, 10-18.R
+++ Code/Table Code/Items 3-5, 10-18.R
@@ -26,7 +26,8 @@ library(data.table)
 # Define File Path
 SPPath   <- "//projects.cadmusgroup.com@SSL/DavWWWRoot/sites/6000-P14/Shared Documents/Analysis/FileMaker Data/Data for SCL"
 cleanInPath <- "//projects.cadmusgroup.com@SSL/DavWWWRoot/sites/6000-P14/Shared Documents/Analysis/FileMaker Data/Analysis Documents/Clean Data"
-stopifnot(all(file.exists(SPPath)))
+analysisInPath <- "//projects.cadmusgroup.com@SSL/DavWWWRoot/sites/6000-P14/Shared Documents/Analysis/FileMaker Data/Analysis Documents"
+stopifnot(all(file.exists(SPPath, cleanInPath, analysisInPath)))
 
 rbsa.dat <- read.xlsx(xlsxFile = file.path(cleanInPath, paste("clean.rbsa.data", rundate, ".xlsx")))
 length(unique(rbsa.dat$CK_Cadmus_ID)) #565
@@ -74,13 +75,31 @@ length(unique(item4.dat2$CK_Cadmus_ID)) #410, boo!
 #make conditioned area as.numeric
 item4.dat2$ConditionedArea <- as.numeric(as.character(item4.dat2$ENV_Construction_BLDG_STRUCTURE_BldgLevel_Area_SqFt))
 
-item4.dat3 <- summarise(group_by(item4.dat2, CK_Cadmus_ID, State)
+#summarise by state
+item4.state.dat <- summarise(group_by(item4.dat2, CK_Cadmus_ID, State)
                       ,siteAreaConditioned = sum(ConditionedArea)
 )
 
-item4.dat4 <- summarise(group_by(item4.dat3, State)
-                        ,AveAreaConditioned = mean(siteAreaConditioned))
+item4.state.dat1 <- summarise(group_by(item4.state.dat, State)
+                        ,AveAreaConditioned = mean(siteAreaConditioned)
+                        ,SEAreaConditioned  = sd(siteAreaConditioned) / sqrt(length(unique(CK_Cadmus_ID)))
+                        ,SampleSize         = length(unique(CK_Cadmus_ID))
+                        )
 
+#summarise by region
+item4.region.dat <- summarise(group_by(item4.dat2, CK_Cadmus_ID)
+                              ,State = "Region"
+                             ,siteAreaConditioned = sum(ConditionedArea)
+)
+
+item4.region.dat1 <- summarise(item4.region.dat
+                               ,State = "Region"
+                              ,AveAreaConditioned = mean(siteAreaConditioned)
+                              ,SEAreaConditioned  = sd(siteAreaConditioned) / sqrt(length(unique(CK_Cadmus_ID)))
+                              ,SampleSize         = length(unique(CK_Cadmus_ID))
+                              )
+
+item4.final <- rbind.data.frame(item4.state.dat1, item4.region.dat1, stringsAsFactors = F) 
 
 #############################################################################################
 # Item 5: AVERAGE CONDITIONED FLOOR AREA BY STATE AND VINTAGE
@@ -99,32 +118,302 @@ length(unique(item5.dat2$CK_Cadmus_ID)) #410, boo!
 #make conditioned area as.numeric
 item5.dat2$ConditionedArea <- as.numeric(as.character(item5.dat2$ENV_Construction_BLDG_STRUCTURE_BldgLevel_Area_SqFt))
 
-item5.dat3 <- summarise(group_by(item5.dat2, CK_Cadmus_ID, State, HomeYearBuilt_bins)
-                        ,siteAreaConditioned = sum(ConditionedArea)
+
+#### By state/region across homeyearbuilt
+#summarise by state
+item5.state.dat00 <- summarise(group_by(item5.dat2, CK_Cadmus_ID, State)
+                             ,siteAreaConditioned = sum(ConditionedArea)
 )
 
-item5.dat4 <- summarise(group_by(item5.dat3, State, HomeYearBuilt_bins)
-                        ,AveAreaConditioned = mean(siteAreaConditioned))
+item5.state.dat01 <- summarise(group_by(item5.state.dat00, State)
+                               ,HomeYearBuilt_bins = "All Vintages"
+                              ,AveAreaConditioned = mean(siteAreaConditioned)
+                              ,SampleSize = length(unique(CK_Cadmus_ID))
+)
+
+#summarise by region
+item5.region.dat00 <- summarise(group_by(item5.dat2, CK_Cadmus_ID)
+                              ,State = "Region"
+                              ,siteAreaConditioned = sum(ConditionedArea)
+)
+
+item5.region.dat01 <- summarise(group_by(item5.region.dat00)
+                               ,State = "Region"
+                               ,HomeYearBuilt_bins = "All Vintages"
+                               ,AveAreaConditioned = mean(siteAreaConditioned)
+                               ,SampleSize = length(unique(CK_Cadmus_ID))
+)
+
+item5.tmp1 <- rbind.data.frame(item5.state.dat01, item5.region.dat01, stringsAsFactors = F) 
+
+
+
+#### By state/region and homeyearbuilt bins
+#summarise by state
+item5.state.dat <- summarise(group_by(item5.dat2, CK_Cadmus_ID, State, HomeYearBuilt_bins)
+                             ,siteAreaConditioned = sum(ConditionedArea)
+)
+
+item5.state.dat1 <- summarise(group_by(item5.state.dat, State, HomeYearBuilt_bins)
+                              ,AveAreaConditioned = mean(siteAreaConditioned)
+                              ,SampleSize = length(unique(CK_Cadmus_ID))
+                              )
+
+#summarise by region
+item5.region.dat <- summarise(group_by(item5.dat2, CK_Cadmus_ID, HomeYearBuilt_bins)
+                              ,State = "Region"
+                              ,siteAreaConditioned = sum(ConditionedArea)
+)
+
+item5.region.dat1 <- summarise(group_by(item5.region.dat, HomeYearBuilt_bins)
+                               ,State = "Region"
+                               ,AveAreaConditioned = mean(siteAreaConditioned)
+                               ,SampleSize = length(unique(CK_Cadmus_ID))
+)
+
+item5.tmp2 <- rbind.data.frame(item5.state.dat1, item5.region.dat1, stringsAsFactors = F) 
+
+
+item5.final <- rbind.data.frame(item5.tmp1, item5.tmp2, stringsAsFactors = F)
+
+
+
+
+
+
 
 
 
 
 
 #############################################################################################
-# Item 10: AVERAGE CONDITIONED FLOOR AREA BY STATE AND BUILDING HEIGHT
+# Item 10: DISTRIBUTION OF FRAME WALL INSULATION LEVELS BY FRAMING TYPE (SF table 17)
 #############################################################################################
 
+#Bring in R-value table
+rvals <- read.xlsx(xlsxFile = file.path(analysisInPath, "R value table.xlsx"), sheet = 1)
+rvals <- rvals[-23,-3]
+
+#subset envelope data to necessary columns
+item10.dat <- envelope.dat[which(colnames(envelope.dat) %in% c("CK_Cadmus_ID"
+                                                               , "Category"
+                                                               , "Wall.Type"
+                                                               , "Wall.Area"
+                                                               , "Wall.Framing.Size"
+                                                               , "Wall.Cavity.Insulated?"
+                                                               , "Wall.Cavity.Insulation.Type.1"
+                                                               , "Wall.Cavity.Insulation.Thickness.1"
+                                                               , "Wall.Cavity.Insulation.Type.2"                                                  
+                                                               , "Wall.Cavity.Insulation.Thickness.2"))]
+length(unique(item10.dat$CK_Cadmus_ID))#547
+
+#subset to only wall information
+item10.dat1 <- item10.dat[which(item10.dat$Category == "Wall"),]
+
+#remove unneccesary wall types -- check with Rietz
+# item10.dat2 <- item10.dat1[which(!(item10.dat1$Wall.Type %in% c("Knee Wall","Masonry","Masonry (Basement)","Log","Adiabatic"))),]
+
+#create "Alternative" category
+item10.dat1$Wall.Type[which(item10.dat1$Wall.Type %in% c("Knee Wall","Masonry","Masonry (Basement)","Log","Adiabatic"))] <- "Alternative"
+length(unique(item10.dat1$CK_Cadmus_ID))#491
+
+# item10.wallType <- unique(item10.dat2[which(colnames(item10.dat2) %in% c("CK_Cadmus_ID", "Wall.Framing.Size"))])
+# item10.wallType2 <- left_join(rbsa.dat, item10.wallType, by = "CK_Cadmus_ID")
+
+#remove items have the datapoint was not asked for
+item10.dat2 <- item10.dat1[which(item10.dat1$Wall.Framing.Size != "-- Datapoint not asked for --"),]
+item10.dat2 <- item10.dat2[which(item10.dat2$`Wall.Cavity.Insulated?` != "Unknown"),]
+
+
+###########################
+# Cleaning Step: Set up unknown and N/A insulation thickness information in order to separate the # from the word "inches" in R
+###########################
+item10.dat2$Wall.Cavity.Insulation.Thickness.1[which(item10.dat2$Wall.Cavity.Insulation.Thickness.1 == "Unknown")] <- "Unknown Unknown"
+item10.dat2$Wall.Cavity.Insulation.Thickness.1[which(item10.dat2$Wall.Cavity.Insulation.Thickness.1 == "N/A")] <- "N/A N/A"
+item10.dat2$Wall.Cavity.Insulation.Thickness.1[which(is.na(item10.dat2$Wall.Cavity.Insulation.Thickness.1))] <- "N/A N/A"
+item10.dat2$Wall.Cavity.Insulation.Thickness.2[which(item10.dat2$Wall.Cavity.Insulation.Thickness.2 == "Unknown")] <- "Unknown Unknown"
+item10.dat2$Wall.Cavity.Insulation.Thickness.2[which(item10.dat2$Wall.Cavity.Insulation.Thickness.2 == "N/A")] <- "N/A N/A"
+item10.dat2$Wall.Cavity.Insulation.Thickness.2[which(is.na(item10.dat2$Wall.Cavity.Insulation.Thickness.2))] <- "N/A N/A"
+
+# add new ID variable for merging -- don't know if we need this
+item10.dat2$count <- 1
+item10.dat2$TMP_ID <- cumsum(item10.dat2$count)
+
+## r-values ##
+clean.insul1 <- unlist(strsplit(item10.dat2$Wall.Cavity.Insulation.Thickness.1, " "))
+clean.insul2 <- as.data.frame(matrix(clean.insul1, ncol = 2, byrow = T), stringsAsFactors = F)
+clean.insul1.1 <- cbind.data.frame("CK_Cadmus_ID" = item10.dat2$CK_Cadmus_ID
+                                   , "TMP_ID" = item10.dat2$TMP_ID
+                                   , clean.insul2)
+dim(clean.insul1.1)
+
+clean.insul2 <- unlist(strsplit(item10.dat2$Wall.Cavity.Insulation.Thickness.2, " "))
+clean.insul2.1 <- cbind.data.frame("CK_Cadmus_ID" = item10.dat2$CK_Cadmus_ID
+                                   , "TMP_ID" = item10.dat2$TMP_ID
+                                   , as.data.frame(matrix(clean.insul2, ncol = 2, byrow = T)
+                                                   , stringsAsFactors = F))
+dim(clean.insul2.1)
+
+clean.insul <- left_join(clean.insul1.1, clean.insul2.1, by = c("CK_Cadmus_ID", "TMP_ID"))
+
+###########################
+# End cleaning step
+###########################
+
+#make into dataframe
+item10.dat3 <- as.data.frame(left_join(item10.dat2, clean.insul, by = c("CK_Cadmus_ID", "TMP_ID"))
+                           , stringsAsFactors = F) 
+# warning here is OK
+
+###########################
+# Cleaning inches and rvalue information
+###########################
+# rename columns
+item10.dat3$inches1 <- as.numeric(as.character(item10.dat3$V1.x)) # warning here is OK
+item10.dat3$inches2 <- as.numeric(as.character(item10.dat3$V1.y)) # warning here is OK
+
+item10.dat3$rvalues1 <- item10.dat3$Wall.Cavity.Insulation.Type.1
+item10.dat3$rvalues2 <- item10.dat3$Wall.Cavity.Insulation.Type.2
+
+#check uniques
+unique(item10.dat3$rvalues1)
+unique(item10.dat3$rvalues2)
+
+#fix names that are not in R value table
+item10.dat3$rvalues1[which(item10.dat3$rvalues1 == "Fiberglass or mineral wool batts")] <- "Mineral wool batts"
+item10.dat3$rvalues1[which(item10.dat3$rvalues1 == "Unknown fiberglass")]               <- "Unknown"
+item10.dat3$rvalues2[which(item10.dat3$rvalues2 == "Extruded polystyrene (blue)")]      <- "Extruded polystyrene foam board"
+item10.dat3$rvalues2[which(item10.dat3$rvalues2 == "N/A")]                              <- NA
+###########################
+# End cleaning step
+###########################
+
+
+###########################
+# Replace R-value Names with Values from R value table
+###########################
+for (i in 1:length(rvals$Type.of.Insulation)){
+  item10.dat3$rvalues1[which(item10.dat3$rvalues1 == rvals$Type.of.Insulation[i])] <- rvals$`Avg..R-Value.Per.Inch`[i]
+  item10.dat3$rvalues2[which(item10.dat3$rvalues2 == rvals$Type.of.Insulation[i])] <- rvals$`Avg..R-Value.Per.Inch`[i]
+}
+###########################
+# End Replace Step
+###########################
+
+item10.dat4 <- item10.dat3
+
+###########################
+# Cleaning step (NA to zero)
+###########################
+#For now: convert NAs in rvalues and inches to zero for summarizing purposes (will revert back to NAs after)
+item10.dat4$rvalues1[which(is.na(item10.dat4$rvalues1))] <- 0
+item10.dat4$rvalues1[which(item10.dat4$rvalues1 == "None")] <- 0
+item10.dat4$rvalues2[which(is.na(item10.dat4$rvalues2))] <- 0
+
+#QC the clean bulb values
+unique(item10.dat4$rvalues1)
+unique(item10.dat4$rvalues2)
+
+item10.dat4$inches1[which(is.na(item10.dat4$inches1))] <- 0
+item10.dat4$inches2[which(is.na(item10.dat4$inches2))] <- 0
+
+#QC the clean bulb values -- There are NAs in each 
+unique(item10.dat4$inches1)
+unique(item10.dat4$inches2)
+###########################
+# End Cleaning step
+###########################
+
+
+# r values multiplied by inches
+item10.dat4$total.r.val <- (as.numeric(as.character(item10.dat4$rvalues1)) * item10.dat4$inches1) #+
+# (as.numeric(as.character(item10.dat4$rvalues2)) * item10.dat4$inches2)
+
+#caluclate u factors = inverse of Rvalue
+item10.dat4$ufactor <- 1 / as.numeric(as.character(item10.dat4$total.r.val))
+item10.dat4$ufactor <- as.numeric(as.character(item10.dat4$ufactor))
+
+# replace inf with 0
+item10.dat4$ufactor[which(item10.dat4$ufactor == "Inf")] <- 0
+
+#make area numeric
+item10.dat4$Wall.Area <- as.numeric(as.character(item10.dat4$Wall.Area))
+
+#weight the u factor per home
+weightedU <- summarise(group_by(item10.dat4, CK_Cadmus_ID)
+                       ,aveUval = sum(Wall.Area * as.numeric(as.character(ufactor))) / sum(Wall.Area)
+)
+
+#back-calculate the weight r values
+weightedU$aveRval <- 1 / as.numeric(as.character(weightedU$aveUval))
+weightedU$aveRval[which(weightedU$aveRval == "Inf")] <- 0
+
+wall.unique <- unique(item10.dat4[,which(colnames(item10.dat4)=="CK_Cadmus_ID" | 
+                                         colnames(item10.dat4)=="BuildingType" |
+                                         colnames(item10.dat4)=="HomeYearBuilt")])
+
+item10.dat5 <- left_join(weightedU, wall.unique, by = "CK_Cadmus_ID")
 
 
 
+############################################################################################################
+## This is a process to determine which Rvalues are NA and which are zero.
+############################################################################################################
+#determine which rvalues were NA and which were 0
+item10.dat5$aveRval[which(item10.dat5$aveRval == 0)] <- NA
+
+#get indicators for ceilings that are not insulated and rvalues that are NA
+wall.ins.ind  <- item10.dat3$CK_Cadmus_ID[which(item10.dat3$`Wall.Cavity.Insulated?` == "No")] 
+wall.r.NA.ind <- item10.dat5$CK_Cadmus_ID[which(is.na(item10.dat5$aveRval))]
+
+#which NA R values are in no insulation?
+no.insulation <- wall.r.NA.ind[which(wall.r.NA.ind %in% wall.ins.ind)]
+
+#replace no insulation in home ceiling with zero
+item10.dat5$aveRval[which(item10.dat5$CK_Cadmus_ID %in% no.insulation)] <- 0
+############################################################################################################
+## END Process
+############################################################################################################
+
+wall.final <- item10.dat5
+
+#Bin R values -- SF only
+wall.final$rvalue.bins <- NA
+wall.final$rvalue.bins[which(wall.final$aveRval == 0)] <- "R0"
+wall.final$rvalue.bins[which(wall.final$aveRval > 0  & wall.final$aveRval < 11)]    <- "R1.R10"
+wall.final$rvalue.bins[which(wall.final$aveRval > 11   & wall.final$aveRval < 17)]  <- "R11.R16"
+wall.final$rvalue.bins[which(wall.final$aveRval > 17  & wall.final$aveRval < 23)]  <- "R17.R22"
+wall.final$rvalue.bins[which(wall.final$aveRval > 23)] <- "RGT22"
+unique(wall.final$rvalue.bins)
+
+##cast data
+wall.final$count <- 1
+item10.dat.cast <- dcast(setDT(wall.final),
+                       formula   = CK_Cadmus_ID + BuildingType + HomeYearBuilt ~ rvalue.bins,
+                       value.var = 'count')
+
+for (i in 3:length(item10.dat.cast)){
+  item10.dat.cast[i][which(is.na(item10.dat.cast[i])),] <- 0
+}
+
+head(item10.dat.cast)
 
 
+## Single Family ## ## Note that there are only 2 MF-Low and 1 MF-High sites, not providing info
+SF.item10.dat <- subset(item10.dat.cast, item10.dat.cast$BuildingType == "Single Family")
 
-
-
-
-
-
+#summarize --SF only
+wall.sum <- summarise(group_by(item10.dat.cast, BuildingType, HomeYearBuilt)
+                      ,sampleSize      = sum(length(unique(CK_Cadmus_ID)))
+                      ,sampleSizeNoNA  = sum(length(unique(CK_Cadmus_ID))) - sum(`NA`)
+                      ,r0.percent      = sum(R0) / sampleSizeNoNA ## note for two houses, there were two ceilings recorded for two sites where one ceiling was not insulated, and one was insulated. Look into automating this.
+                      ,r1.r10.percent  = sum(R1.R10)  / sampleSizeNoNA
+                      ,r11.r16.percent = sum(R11.R16) / sampleSizeNoNA
+                      ,r17.r22.percent = sum(R17.R22) / sampleSizeNoNA
+                      ,rGT22.percent   = 0#sum(RGT22) / sampleSizeNoNA
+)
+#Check to make sure they add to 1, 2, 3, or 4
+sum(wall.sum[,c(5:9)])
 
 
 
