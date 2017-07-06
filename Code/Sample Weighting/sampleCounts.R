1dc9891fe08cd6ce091aecd9e60a0948d10856f6
Andrew Bernath <Andrew.Bernath@cadmusgroup.com>
2017-07-06 11:04:27 -0700

    7/6/2017 - Small update to file

--- Code/Sample Weighting/sampleCounts.R
+++ Code/Sample Weighting/sampleCounts.R
@@ -31,30 +31,41 @@ library(data.table)
 rootpath <- "//projects.cadmusgroup.com@SSL/DavWWWRoot/sites/6000-P14/Shared Documents/Analysis/FileMaker Data"
 analysisFolder <- rootpath
 meterDataPath  <- file.path(analysisFolder,"Data for PSE")
-weightDataPath <- file.path(analysisFolder,"Weighting Files")     ##  Also output folder
+bldgDataPath   <- file.path(analysisFolder,"Data for PSE")
+weightDataPath <- file.path(analysisFolder,"Weight Source")     ##  Also output folder
 
 stopifnot(all(file.exists(rootpath, analysisFolder, meterDataPath, weightDataPath)))
 
 # Call file names
 meter.export  <- "METERS_2017.06.16.xlsx"
 popZIP.export <- "ZIP_Code_Utility_Mapping.xlsx"
-
+bldg.export   <- "SITES_2017.06.16.xlsx"
 
 #############################################################################################
 # Import and Subset Data
 #############################################################################################
 
-# Import site data (Items 1, 2, 3-maybe)
-site.dat <- read.xlsx(xlsxFile = file.path(filepathRawData, meter.export), sheet=1)
-names(site.dat)
-site.dat1 <- data.frame("CK_Cadmus_ID" = site.dat$CK_Cadmus_ID
-                        , "PK_Site_ID"      = site.dat$PK_SiteID
-                        , "ZIPCode"         = site.dat$SITE_ZIP
-                        , stringsAsFactors  = F)
-head(site.dat1)
-site.dat1$CK_Cadmus_ID <- trimws(toupper(site.dat1$CK_Cadmus_ID))
-site.dat1$PK_Site_ID   <- trimws(toupper(site.dat1$PK_Site_ID))
-site.dat1$ZIPCode      <- substr(site.dat1$ZIPCode, 1, 5)  ## Remove ZIP-Ext
-length(unique(site.dat1$CK_Cadmus_ID)) #601
+# Import ID and ZIP data
+id_zip.dat <- read.xlsx(xlsxFile = file.path(meterDataPath, meter.export), sheet=1)
+names(id_zip.dat)
+id_zip.dat1 <- data.frame("CK_Cadmus_ID"     = id_zip.dat$CK_Cadmus_ID
+                          , "PK_Site_ID"     = id_zip.dat$PK_SiteID
+                          , "ZIPCode"        = id_zip.dat$SITE_ZIP
+                          , stringsAsFactors = F)
+id_zip.dat1$CK_Cadmus_ID <- trimws(toupper(id_zip.dat1$CK_Cadmus_ID))
+id_zip.dat1$PK_Site_ID   <- trimws(toupper(id_zip.dat1$PK_Site_ID))
+id_zip.dat1$ZIPCode      <- substr(id_zip.dat1$ZIPCode, 1, 5)  ## Remove ZIP-Ext
+length(unique(id_zip.dat1$CK_Cadmus_ID))  ## 567 unique respondent ID's
+length(unique(id_zip.dat1$PK_Site_ID))  ## 574 unique site ID's
+
+
+# Import bldg type data
+bldg.dat <- read.xlsx(xlsxFile = file.path(bldgDataPath, bldg.export), sheet=1)
+names(bldg.dat)
+bldg.dat1 <- data.frame("PK_Site_ID"       = bldg.dat$PK_SiteID
+                        , "BuildingType"   = bldg.dat$bldg_type
+                        , stringsAsFactors = F)
+bldg.dat1$PK_Site_ID   <- trimws(toupper(bldg.dat1$PK_Site_ID))
+length(unique(bldg.dat1$PK_Site_ID))  ## 567 unique ID's
 
 
