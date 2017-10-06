0dd7b7b0f6490236bc53d702d0dcdfdc74a65737
Andres Roma <Andres.Roma@cadmusgroup.com>
2017-10-06 14:16:41 -0700

    Adding code for items 145 and 149, EUI by heating fuel type

---
+++ Code/Table Code/Items 145 and 149.R
@@ -0,0 +1,21 @@
+#############################################################################################
+##  Title:            RBSA Analysis                      
+##  Author:           Casey Stevens, Cadmus Group               
+##  Created:          06/13/2017
+##  Updated:                                             
+##  Billing Code(s):  
+#############################################################################################
+
+
+##  Clear variables
+rm(list=ls())
+rundate <-  format(Sys.time(), "%d%b%y")
+options(scipen=999)
+
+source("Code/Table Code/SourceCode.R")
+
+#########################
+
+## Imported Weighted RBSA data- this file currentyl has fake usages maybe wont in future
+rbsa.dat <- read.xlsx(xlsxFile = file.path(filepathCleanData, paste("clean.rbsa.data", rundate, ".xlsx", sep = "")))
+length(unique(rbsa.dat$CK_Cadmus_ID)) #601
