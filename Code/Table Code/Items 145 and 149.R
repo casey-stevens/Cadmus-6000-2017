835662de997071b8b7c06ba13d797fff997c85c3
Andres Roma <Andres.Roma@cadmusgroup.com>
2017-10-09 13:27:05 -0700

    Code for item 145 all set up but openxlsx not working

--- Code/Table Code/Items 145 and 149.R
+++ Code/Table Code/Items 145 and 149.R
@@ -5,6 +5,40 @@
 ##  Updated:                                             
 ##  Billing Code(s):  
 #############################################################################################
+rbsa.dat$kWh_usage_fake <- ceiling(runif(n = nrow(rbsa.dat),
+                                               min = 3000, 
+                                               max = 30000))
+
+rbsa.dat$kWh_NAC_fake <- rbsa.dat$kWh_usage_fake * runif(n = nrow(rbsa.dat),
+                                                                     min = .8,
+                                                                     max = .95)
+
+rbsa.dat$Heating_kWh_fake <- rbsa.dat$kWh_NAC_fake * runif(n = nrow(rbsa.dat),
+                                                                       min = .05,
+                                                                       max = .8)
+
+rbsa.dat$Heating_kWh_fake[which(rbsa.dat$Heating_kWh_fake < 3000)] <- 0
+rbsa.dat$Cooling_kWh_fake <- rbsa.dat$kWh_NAC_fake * runif(n = nrow(rbsa.dat),
+                                                                       min = .05,
+                                                                       max = .5)
+rbsa.dat$Cooling_kWh_fake[which(rbsa.dat$Cooling_kWh_fake < 1500)] <- 0
+
+rbsa.dat$Heating_kWh_fake[which(rbsa.dat$kWh_NAC_fake -
+                                        rbsa.dat$Heating_kWh_fake - 
+                                        rbsa.dat$Cooling_kWh_fake < 0)] <- 0
+
+
+rbsa.dat$therm_usage_fake <- ceiling(runif(n = nrow(rbsa.dat),
+                                                 min = 200, 
+                                                 max = 3000))
+rbsa.dat$therm_NAC_fake <- rbsa.dat$therm_usage_fake * runif(n = nrow(rbsa.dat),
+                                                                         min = .8,
+                                                                         max = .95)
+
+rbsa.dat$Heating_therm_fake <- rbsa.dat$therm_NAC_fake * runif(n = nrow(rbsa.dat),
+                                                                           min = .1,
+                                                                           max = .9)
+rbsa.dat$Heating_therm_fake[which(rbsa.dat$Heating_therm_fake < 300)] <- 0
 
 
 ##  Clear variables
@@ -17,7 +51,7 @@ source("Code/Table Code/SourceCode.R")
 #########################
 
 ## Imported Weighted RBSA data- this file currentyl has fake usages maybe wont in future
-rbsa.dat <- read.xlsx(xlsxFile = file.path(filepathCleanData, paste("clean.rbsa.data", rundate, ".xlsx", sep = "")))
+rbsa.dat <- read.xlsx(xlsxFile = file.path(filepathCleanData, paste("clean.rbsa.data", '06Oct17', ".xlsx", sep = "")))
 length(unique(rbsa.dat$CK_Cadmus_ID)) #601
 
 ## Import Mechanical Data for Heating Fuel Type
@@ -93,6 +127,7 @@ item145.dat$FuelType[which(item145.dat$Heating.Fuel %in% c("Wood (cord)",
                                                            "Natural Gas",
                                                            "Natural gas"))] <- "Other"
 
+item145.dat$FuelType[which(item145.dat$Heating.Fuel == 'Electric')] <- 'Electric'
 item145.dat$EUI <- item145.dat$kWh_NAC_fake/item145.dat$SqFt
 
 ######################################################
@@ -139,6 +174,11 @@ item145.state.all <- summarise(group_by(item145.strata.all, BuildingType, State)
 
 item145.state <- rbind.data.frame(item145.state.fuel, item145.state.all,
                                   stringsAsFactors = F)
+
+item145.state.table <- dcast(setDT(item145.state)
+                       ,formula = BuildingType + State ~ FuelType
+                       ,value.var = c("Mean", "SE", "SampleSize"))
+
 ######################################################
 # Step 3: Using strata level data,
 #   Perform region level analysis
@@ -157,15 +197,40 @@ item145.region.all <- summarise(group_by(item145.strata.all, BuildingType)
 
 item145.region <- rbind.data.frame(item145.region.fuel, 
                                    item145.region.all, stringsAsFactors = F)
+
+item145.region.table <- dcast(setDT(item145.region)
+                             ,formula = BuildingType + State ~ FuelType
+                             ,value.var = c("Mean", "SE", "SampleSize"))
+
 ######################################################
 # Step 4: Combine results into correct table format,
 #   Split table by building type
 #   and export tables to respective workbooks
 ######################################################
-item145.final <- rbind.data.frame(item145.state, item145.region, stringsAsFactors = F)
-item145.table.SF <- item145.final[which(item145.final$BuildingType %in% c("Single Family")),-1]
-item145.table.MH <- item145.final[which(item145.final$BuildingType %in% c("Manufactured")),-1]
+item145.final <- rbind.data.frame(item145.state.table, item145.region.table, stringsAsFactors = F)
 
-item145.table.SF2 <- dcast(setDT(item145.table.SF)
-                      ,formula = State ~ FuelType
-                      ,value.var = c("Mean", "SE", "SampleSize"))
+
+item145.table <- data.frame("BuildingType" = item145.final$BuildingType,
+                            "State" = item145.final$State,
+                            "Homes With Electric Heat" = item145.final$Mean_Electric,
+                            "SE_Electric" = item145.final$SE_Electric,
+                            "Homes With Other Heat" = item145.final$Mean_Other,
+                            "SE_Other" = item145.final$SE_Other,
+                            "All Homes" = item145.final$`Mean_All Homes`,
+                            "SE_All" = item145.final$`SE_All Homes`,
+                            "SampleSize" = item145.final$`SampleSize_All Homes`)
+item145.table.SF <- item145.table[which(item145.final$BuildingType %in% c("Single Family")),-1]
+
+library(openxlsx)
+Sys.setenv("R_ZIPCMD" = "C:/Rtools/bin/zip.exe")
+workbook.SF <- loadWorkbook(file = paste(outputFolder, "Tables in Excel - SF - COPY.xlsx", sep="/"))
+workbook.MH <- loadWorkbook(file = paste(outputFolder, "Tables in Excel - MH - COPY.xlsx", sep="/"))
+
+# UPDATE SHEET AND X
+writeData(workbook.SF, sheet = "Table 153", x = item150.table.SF, startRow = 20)
+writeData(workbook.MH, sheet = "Table 128", x = item150.table.MH, startRow = 20)
+
+saveWorkbook(workbook.SF, file = paste(outputFolder, "Tables in Excel - SF - COPY.xlsx", sep="/"), overwrite = T)
+saveWorkbook(workbook.MH, file = paste(outputFolder, "Tables in Excel - MH - COPY.xlsx", sep="/"), overwrite = T)
+
+
