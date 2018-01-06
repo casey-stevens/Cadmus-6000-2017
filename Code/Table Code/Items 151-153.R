58d35f775e582c2cdf2c4094e1cdc27a3aee692c
Andres Roma <Andres.Roma@cadmusgroup.com>
2018-01-05 16:07:20 -0800

    Item 154, 155 committed and completed Item 151-153, update to exclude gas heating with no gas usage data

--- Code/Table Code/Items 151-153.R
+++ Code/Table Code/Items 151-153.R
@@ -98,7 +98,7 @@ final.data <- unique(left_join(results.dat2, mecahnical.dat.final))
 ##################################################################################################
 item151.dat1 <- final.data[which(final.data$UsageNAC_kWh > 0), ]
 item151.dat2 <- item151.dat1[-which(is.na(item151.dat1$UsageNAC_therms) &
-                                      item151.dat1$Heating_Fuel != "Electric"),]
+                                      item151.dat1$Heating_Fuel == "Gas"),]
 item151.dat2$UsageNAC_therms[which(is.na(item151.dat2$UsageNAC_therms))] <- 0
 
 item151.dat2$kBtu <- item151.dat2$UsageNAC_kWh * 3.412 + item151.dat2$UsageNAC_therms * 99.98
@@ -155,12 +155,12 @@ exportTable(item151.final.MH, "MH", "Table 133", weighted = FALSE)
 #############################################################################################
 # Item 152: AVERAGE ELECTRICITY AND GAS EUI BY STATE - SF TABLE 159, MH TABLE 134
 #############################################################################################
-item152.dat <- final.data[which(final.data$UsageNAC_kWh > 0),]
-item152.dat2 <- item152.dat[-which(is.na(item152.dat$UsageNAC_therms) &
+item152.dat <- final.data[which(final.data$UsageRaw_kWh > 0),]
+item152.dat2 <- item152.dat[-which(is.na(item152.dat$UsageRaw_therms) &
                                      item152.dat$Heating_Fuel != "Electric"),]
 item152.dat3 <- item152.dat2[which(item152.dat2$Conditioned.Area > 0),]
-item152.dat3$UsageNAC_therms[which(is.na(item152.dat3$UsageNAC_therms))] <- 0
-item152.dat3$kBtu <- item152.dat3$UsageNAC_kWh * 3.412 + item152.dat3$UsageNAC_therms * 99.98
+item152.dat3$UsageRaw_therms[which(is.na(item152.dat3$UsageRaw_therms))] <- 0
+item152.dat3$kBtu <- item152.dat3$UsageRaw_kWh * 3.412 + item152.dat3$UsageRaw_therms * 99.98
 item152.dat3$EUI <- item152.dat3$kBtu/item152.dat3$Conditioned.Area
 
 
---
+++ Code/Table Code/Items 154, 155.R
@@ -0,0 +1,162 @@
+#############################################################################################
+##  Title:            RBSA Analysis                      
+##  Author:           Casey Stevens, Cadmus Group               
+##  Created:          06/13/2017
+##  Updated:                                             
+##  Billing Code(s):  
+#############################################################################################
+##  Clear variables
+
+##  Clear variables
+rm(list = ls())
+rundate <-  format(Sys.time(), "%d%b%y")
+options(scipen = 999)
+
+##  Create "Not In" operator
+"%notin%" <- Negate("%in%")
+
+# Source codes
+source("Code/Table Code/SourceCode.R")
+source("Code/Table Code/Weighting Implementation Functions.R")
+source("Code/Sample Weighting/Weights.R")
+source("Code/Table Code/Export Function.R")
+
+# Read in clean RBSA data
+rbsa.dat <- read.xlsx(xlsxFile = file.path(filepathCleanData, paste("clean.rbsa.data", rundate, ".xlsx", sep = "")))
+length(unique(rbsa.dat$CK_Cadmus_ID))
+rbsa.dat2 <- rbsa.dat[-grep("bldg",rbsa.dat$CK_Building_ID, ignore.case = T),]
+
+one.line.dat  <- read.xlsx(xlsxFile = file.path(filepathRawData, one.line.export), 
+                           sheet = "Site One Line Summary", startRow = 2)
+
+colnames(one.line.dat)
+keep.cols <- c("Cadmus.ID", 
+               "Annual.Wood.Cord.Usage.(Cords)",
+               "Annual.Wood.Pellet.Usage.(Tons)",
+               "Annual.Propane.Usage.(Gallons)",
+               "Annual.Oil.Usage.(Gallons)")
+one.line.dat2 <- one.line.dat[,which(colnames(one.line.dat) %in% keep.cols)]
+one.line.dat2$CK_Cadmus_ID <- trimws(toupper(one.line.dat2$Cadmus.ID))
+final.dat <- merge(rbsa.dat2, one.line.dat2,
+                   by = "CK_Cadmus_ID",
+                   all.x = T)
+
+#############################################################################################
+# Item 154: TABLE 161: AVERAGE ANNUAL OTHER FUEL USE PER HOME BY STATE 
+#############################################################################################
+item154.data <- final.dat
+item154.data$kBtu <- 
+  item154.data$`Annual.Wood.Cord.Usage.(Cords)` * 18000 +
+  item154.data$`Annual.Wood.Pellet.Usage.(Tons)` * 16000 +
+  item154.data$`Annual.Oil.Usage.(Gallons)` * 140 +
+  item154.data$`Annual.Propane.Usage.(Gallons)` * 92
+  
+drop.for.weighting <- c("Cadmus.ID",
+                        "Annual.Wood.Cord.Usage.(Cords)",
+                        "Annual.Wood.Pellet.Usage.(Tons)",
+                        "Annual.Propane.Usage.(Gallons)",
+                        "Annual.Oil.Usage.(Gallons)",
+                        "kBtu")
+
+item154.weighted <- weightedData(item154.data[-c(which(colnames(item154.data) %in% 
+                                                         drop.for.weighting))])
+item154.weighted <- 
+  left_join(item154.weighted, item154.data[c(1,
+                                             c(which(colnames(item154.data) %in% 
+                                                       drop.for.weighting)))])
+
+################################
+# Weighted Analysis1
+################################
+item154.final <- mean_one_group(CustomerLevelData = item154.weighted
+                                , valueVariable = 'kBtu'
+                                , byVariable    = 'State'
+                                , aggregateRow  = 'Region')
+
+#subset by home type
+item154.final.SF <- item154.final[which(item154.final$BuildingType == "Single Family"),-1]
+item154.final.MH <- item154.final[which(item154.final$BuildingType == "Manufactured"),-1]
+##export data - haven't changed any of the datsets waiting for Casey to QC
+#exportTable(item154.final.SF, "SF", "Table 161"
+#            , weighted = TRUE)
+#exportTable(item154.final.MH, "MH", "Table 125"
+#            , weighted = TRUE)
+
+################################
+# Unweighted Analysis
+################################
+item154.final <- mean_one_group_unweighted(CustomerLevelData = item154.weighted
+                                           , valueVariable = 'UsageRaw_kWh'
+                                           , byVariable    = 'State'
+                                           , aggregateRow  = 'Region')
+#subset by home type
+item154.final.SF <- item154.final[which(item154.final$BuildingType == "Single Family"),-1]
+item154.final.MH <- item154.final[which(item154.final$BuildingType == "Manufactured"),-1]
+#export data
+#exportTable(item154.final.SF, "SF", "Table 150"
+#            , weighted = FALSE)
+#exportTable(item154.final.MH, "MH", "Table 125"
+#            , weighted = FALSE)
+
+#############################################################################################
+# Item 155: TABLE 162: AVERAGE EUI OTHER FUEL USE PER HOME BY STATE 
+#############################################################################################
+item155.data <- final.dat
+item155.data$kBtu <- 
+  item155.data$`Annual.Wood.Cord.Usage.(Cords)` * 18000 +
+  item155.data$`Annual.Wood.Pellet.Usage.(Tons)` * 16000 +
+  item155.data$`Annual.Oil.Usage.(Gallons)` * 140 +
+  item155.data$`Annual.Propane.Usage.(Gallons)` * 92
+item155.data1 <- item155.data[which(item155.data$Conditioned.Area > 0),]
+item155.data1$EUI <- item155.data1$kBtu/item155.data1$Conditioned.Area
+
+drop.for.weighting <- c("Cadmus.ID",
+                        "Annual.Wood.Cord.Usage.(Cords)",
+                        "Annual.Wood.Pellet.Usage.(Tons)",
+                        "Annual.Propane.Usage.(Gallons)",
+                        "Annual.Oil.Usage.(Gallons)",
+                        "kBtu",
+                        "EUI")
+
+item155.weighted <- weightedData(item155.data1[-c(which(colnames(item155.data1) %in% 
+                                                         drop.for.weighting))])
+item155.weighted <- 
+  left_join(item155.weighted, item155.data1[c(1,
+                                             c(which(colnames(item155.data1) %in% 
+                                                       drop.for.weighting)))])
+
+################################
+# Weighted Analysis1
+################################
+item155.final <- mean_one_group(CustomerLevelData = item155.weighted
+                                , valueVariable = 'EUI'
+                                , byVariable    = 'State'
+                                , aggregateRow  = 'Region')
+
+#subset by home type
+item155.final.SF <- item155.final[which(item155.final$BuildingType == "Single Family"),-1]
+item155.final.MH <- item155.final[which(item155.final$BuildingType == "Manufactured"),-1]
+#export data - haven't changed any of the datsets waiting for Casey to QC
+#exportTable(item155.final.SF, "SF", "Table 161"
+#            , weighted = TRUE)
+#exportTable(item155.final.MH, "MH", "Table 125"
+#            , weighted = TRUE)
+
+################################
+# Unweighted Analysis
+################################
+item155.final <- mean_one_group_unweighted(CustomerLevelData = item155.weighted
+                                           , valueVariable = 'UsageRaw_kWh'
+                                           , byVariable    = 'State'
+                                           , aggregateRow  = 'Region')
+#subset by home type
+item155.final.SF <- item155.final[which(item155.final$BuildingType == "Single Family"),-1]
+item155.final.MH <- item155.final[which(item155.final$BuildingType == "Manufactured"),-1]
+#export data
+#exportTable(item155.final.SF, "SF", "Table 150"
+#            , weighted = FALSE)
+#exportTable(item155.final.MH, "MH", "Table 125"
+#            , weighted = FALSE)
+
+
+
