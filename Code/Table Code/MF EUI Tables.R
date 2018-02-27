f91a3ad1309db09b207b3732fd649678a32dbfc5
Andres Roma <Andres.Roma@cadmusgroup.com>
2018-02-26 18:05:22 -0800

    Close need to specify the actual columns I need though from the building file and the billing file

--- Code/Table Code/MF EUI Tables.R
+++ Code/Table Code/MF EUI Tables.R
@@ -40,6 +40,28 @@ billing.combined <- merge(x = billing.dat,
                           y = building.summary,
                           by = "PK_BuildingID",
                           all.x = T)
-
 # Did any not merge?
 stopifnot(length(which(is.na(billing.combined$BuildingFlag))) == 0)
+
+# Merge on the RBSA file 
+rbsa.dat <- read.xlsx(xlsxFile = file.path(filepathCleanData, paste("clean.rbsa.data", rundate, ".xlsx", sep = "")))
+rbsa.dat$rbsaFlag <- 1
+
+billing.combined2 <- merge(x = billing.combined, 
+                           y = rbsa.dat,
+                           by.x = "PK_BuildingID",
+                           by.y = "CK_Building_ID",
+                           all.x = T)
+# Did any not merge?
+stopifnot(length(which(is.na(rbsa.dat$rbsaFlag))) == 0)
+
+
+###################################################################################################################
+# ITEM 305: AVERAGE ANNUAL UNIT ELECTRIC CONSUMPTION BY BUILDING SIZE (Table 99)
+###################################################################################################################
+
+item305.dat1 <- 
+  billing.combined[which(billing.combined2$Average.Unit.kWh.Usage > 0),]
+
+item305.data <- weightedData(item305.dat1[which(colnames(item305.dat1) %notin% c("PK_BuildingID"
+                                                                                 ,"Average.Unit.kWh.Usage"))])
